80a6edc30aa6621d393e6cbbe884339b08b7af02
Julian Knight <jk.github@totallyinformation.com>
2016-05-17 17:12:29 +0100

    Update AAD-GetAllUsers.ps1
    Improvements to error handling & updated/improved help information.

--- scripts/public/AAD-GetAllUsers.ps1
+++ scripts/public/AAD-GetAllUsers.ps1
@@ -1,31 +1,56 @@
 <#
 .SYNOPSIS 
-    Dump all AAD entries to a CSV for further processing
+    Dump all AAD entries to a CSV for further processing, save the CSV in files uploaded to SharePoint
 
 .DESCRIPTION
+    Dumps out all Azure Active Directory entries, expanding those that are normally buried in nexted objects so
+    that they are more usable in downstream analytics.
+
+    The file is then uploaded to a SharePoint document library as a latest and a dated version.
+
+.INPUTS
+    None
+
+.OUTPUTS
+    2 CSV Files in the specificed SharePoint document library, one marked as fname-Latest.csv and the other fname-yyyymmdd.csv
+
+.LINK
+    https://github.com/nhsengland/ICTAzureAutomation/blob/master/scripts/public/AAD-GetAllUsers.ps1
 
 .NOTES
+    If you need to detect whether running locally (with the ISE addin), either check for the $psISE variable
+    or check if the machine="Client" and/or the user="Client".
+
+    PREREQUISITES
+    - Azure Automation
+    - Azure AD (MSOL) PowerShell Module
+    - Office 365 PnP PowerShell Module
+    - Azure Automation Assets:
+      - Login Credential with suitable rights to AAD and SP
+      - Various variables holding domain/tenant and other more sensitive information - so no need to keep them in this script
+    - Azure Automation ISE add-on (if you want to test locally) [Install-Module -Name AzureAutomationAuthoringToolkit; Install-AzureAutomationIseAddOn;]
+
     TODO
     - Add group membership checks (NSHE and NHSE Partners at least)
     - List users admin roles
     - Get services from licenses other than E3
 
-    NOTE: This script must be run under Azure Automation Only
+    NOTE: This script should be run under Azure Automation
+          However, it does work locally if you have the AzureAutomationAuthoringToolkit module installed and active
     --------------------------------------------------------------------------------
     Author: Julian Knight, Head of Corporate ICT Technology & Security, NHS England
     History:
+        2016-05-14: Fixed, uploads to SP, works locally and under AA
         2016-05-05: Initial Version
  #>
 
-$VerbosePreference = 'Continue' # Uncomment to get verbose output
+#$VerbosePreference = 'Continue' # Uncomment to get verbose output
 
 $invocation = (Get-Variable MyInvocation).Value
 $cmdName = $invocation.MyCommand.Name
 $strt = get-date
 
-Write-Verbose ""
-Write-Verbose "Starting $cmdName at $strt"
-Write-Verbose ""
+Write-Output "Starting $cmdName at $strt"
 
 #region "Setup" # ----------------------------------------------------------------
 
@@ -39,6 +64,21 @@ $aadCustomDomain2 = Get-AutomationVariable -Name 'aadCustomDomain2'          # a
 $aadCustomDomain2Name = Get-AutomationVariable -Name 'aadCustomDomain2Name'  # A display name to match with the custom domain2
 
 $usersOut = New-Object System.Collections.Generic.List[System.Object]
+
+$ICTo365AuditSite = Get-AutomationVariable -Name 'ICT-o365AuditSite'         # The site to upload output CSV files to
+$ICTo365AuditFolder = Get-AutomationVariable -Name 'ICT-o365AuditFolder'     # The folder to upload output CSV files to
+
+# Temporary output & file names
+$outPath = $env:TEMP   # C:\Users\Client\Temp
+$outFile = Join-Path -Path $outPath -ChildPath "AAD-AllUsers-Latest.csv"
+$DateStr = Get-Date -format "yyyyMMdd" # Get current date for adding to file names
+$outFileDated = Join-Path -Path $outPath -ChildPath "AAD-AllUsers-$DateStr.csv"
+
+# Check if any of the temporary CSV files exist
+if ( Test-Path -Path (Join-Path -Path $outPath -ChildPath "*.csv") ) {
+    # CSV files exist in the output path so this script is looping!
+    Throw "CSV files exist in output folder - script is looping!"
+}
 
 #endregion "Setup" # -------------------------------------------------------------
 
@@ -101,11 +141,11 @@ function Track-LicensesAndServices {
     Param (
         # User Object from MSOL-GetUser
         [parameter(Mandatory=$true)]
-        [Microsoft.Online.Administration.UserLicense]$licenses,
+        $licenses, #[Microsoft.Online.Administration.UserLicense]
 
         # Updated User Object
         [parameter(Mandatory=$true)]
-        [Microsoft.Online.Administration.User]$cols
+        $cols # [Microsoft.Online.Administration.User]
     )
 
     for ($i=0; $i -lt $($licenses.count); $i++) {
@@ -113,8 +153,39 @@ function Track-LicensesAndServices {
 
         # Note that $licenses = $user.Licenses is an object, $cols.Licenses is a ;-separated string
 
-        # E3
-        if ($cols.Licenses -match ':ENTERPRISEPACK') {
+        #AAD Basic
+        if ($license.AccountSkuId -match 'AAD_BASIC') {
+            # Service Names: AAD_BASIC
+            $cols.IsAADb = $true
+        }
+
+        #CRMENTERPRISE IsCrmEnt
+        if ($license.AccountSkuId -match 'CRMENTERPRISE') {
+            # Service Names: CRMENTERPRISE, DMENTERPRISE, NBENTERPRISE, PARATURE_ENTERPRISE
+            $cols.IsCrmEnt = $true
+        }
+
+        # NB: CRMINSTANCE: Not interested with that as it isn't user level licensing
+
+        #CRMPLAN2 IsCrmP2
+        if ($license.AccountSkuId -match 'CRMPLAN2') {
+            # Service Names: CRMPLAN2
+            $cols.IsCrmP2 = $true
+        }
+
+        #CRMSTANDARD IsCrmStd
+        if ($license.AccountSkuId -match 'CRMSTANDARD') {
+            # Service Names: CRMSTANDARD, MDM_SALES_COLLABORATION, NBPROFESSIONALFORCRM
+            $cols.IsCrmStd = $true
+        }
+
+        # NB: CRMSTORAGE: Not interested with that as it isn't user level licensing
+        # NB: CRMTESTINSTANCE: Not interested with that as it isn't user level licensing
+
+        #ENTERPRISEPACK (E3)
+        if ($license.AccountSkuId -match 'ENTERPRISEPACK') {
+            # Service Names: EXCHANGE_S_ENTERPRISE, INTUNE_O365, MCOSTANDARD, OFFICESUBSCRIPTION, RMS_S_ENTERPRISE, SHAREPOINTENTERPRISE, SHAREPOINTWAC, SWAY, YAMMER_ENTERPRISE
+
             $cols.IsE3 = $true
 
             # Find current users service plans & provisioning status
@@ -125,6 +196,9 @@ function Track-LicensesAndServices {
                 if ( ($servicePlan.ServicePlan.ServiceName -eq 'EXCHANGE_S_ENTERPRISE') ) {
                     $cols.HasExchange = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
                 }
+                if ( ($servicePlan.ServicePlan.ServiceName -eq 'INTUNE_O365') ) {
+                    $cols.HasIntune = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
+                }                
                 if ( ($servicePlan.ServicePlan.ServiceName -eq 'MCOSTANDARD') ) {
                     $cols.HasLync = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
                 }
@@ -146,60 +220,46 @@ function Track-LicensesAndServices {
                 if ( ($servicePlan.ServicePlan.ServiceName -eq 'YAMMER_ENTERPRISE') ) {
                     $cols.HasYam = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
                 }
-                if ( ($servicePlan.ServicePlan.ServiceName -eq 'INTUNE_O365') ) {
-                    $cols.HasIntune = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
-                }                
             }
         }
 
-        # SharePoint P2
-        if ($cols.Licenses -match 'SHAREPOINTENTERPRISE') {
-            $cols.IsP2 = $true
-            $cols.HasSP = 'Y'
-        }
-
-        # AAD Basic
-        if ($cols.Licenses -match 'AAD_BASIC') {
-            $cols.IsAADb = $true
-        }
-
-        #CRMENTERPRISE IsCrmEnt
-        if ($cols.Licenses -match 'CRMENTERPRISE') {
-            $cols.IsCrmEnt = $true
-        }
-
-        #CRMPLAN2 IsCrmP2
-        if ($cols.Licenses -match 'CRMPLAN2') {
-            $cols.IsCrmP2 = $true
-        }
-
-        #CRMSTANDARD IsCrmStd
-        if ($cols.Licenses -match 'CRMSTANDARD') {
-            $cols.IsCrmStd = $true
-        }
-
         #PLANNERSTANDALONE IsPlan
-        if ($cols.Licenses -match 'PLANNERSTANDALONE') {
+        if ($license.AccountSkuId -match 'PLANNERSTANDALONE') {
             $cols.IsPlan = $true
         }
 
         #POWER_BI_PRO IsBiPro
-        if ($cols.Licenses -match 'POWER_BI_PRO') {
+        if ($license.AccountSkuId -match 'POWER_BI_PRO') {
             $cols.IsBiPro = $true
         }
 
         #POWER_BI_STANDARD IsBiStd
-        if ($cols.Licenses -match 'POWER_BI_STANDARD') {
+        if ($license.AccountSkuId -match 'POWER_BI_STANDARD') {
             $cols.IsBiStd = $true
         }
 
         #PROJECTCLIENT IsProj
-        if ($cols.Licenses -match 'PROJECTCLIENT') {
+        if ($license.AccountSkuId -match 'PROJECTCLIENT') {
             $cols.IsProj = $true
         }
 
+        #SHAREPOINTENTERPRISE (SharePoint P2)
+        if ($license.AccountSkuId -match 'SHAREPOINTENTERPRISE') {
+            # Service Names: INTUNE_O365, SHAREPOINTENTERPRISE
+
+            $cols.IsP2 = $true
+
+            if ( ($servicePlan.ServicePlan.ServiceName -eq 'SHAREPOINTENTERPRISE') ) {
+                $cols.HasSP = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
+            }
+
+            if ( ($servicePlan.ServicePlan.ServiceName -eq 'INTUNE_O365') ) {
+                $cols.HasIntune = Show-ServiceStatus -status $servicePlan.ProvisioningStatus
+            }                
+        }
+
         #VISIOCLIENT IsVisio
-        if ($cols.Licenses -match 'VISIOCLIENT') {
+        if ($license.AccountSkuId -match 'VISIOCLIENT') {
             $cols.IsVisio = $true
         }
 
@@ -248,12 +308,12 @@ function Build-UserRow {
         [Microsoft.Online.Administration.User]$user
     )
 
-    # We want most of the AAD user object but some bits need expanding, also get rid of rubbish
-    $cols = $user | Select-Object -ExcludeProperty `
-        AlternateEmailAddresses,AlternateMobilePhones,Licenses,AlternativeSecurityIds,ServiceInformation,ProxyAddresses,
+    # We want most of the AAD user object but some bits need expanding, also get rid of rubbish (note that you HAVE to use -Property * or the exclude doesn't work!
+    $cols = $user | Select-Object -Property * -ExcludeProperty `
+        AlternateEmailAddresses,AlternateMobilePhones,Licenses,AlternativeSecurityIds,ServiceInformation,ProxyAddresses,DirSyncProvisioningErrors,
         Errors,ExtensionData,ImmutableId,IndirectLicenseErrors,IsBlackberryUser,LiveId,PasswordResetNotRequiredDuringActivate,
         StrongAuthenticationMethods,StrongAuthenticationPhoneAppDetails,StrongAuthenticationProofupTime,StrongAuthenticationRequirements,StrongAuthenticationUserDetails
-
+    
     #region "ExtraCols"
     # Flatten some of the more complex data for easier processing
     Add-Member -InputObject $cols -MemberType NoteProperty -Name AlternateEmailAddresses -Value ($user.AlternateEmailAddresses -join ';')
@@ -262,14 +322,23 @@ function Build-UserRow {
     Add-Member -InputObject $cols -MemberType NoteProperty -Name AltSecurityIDs -Value ($user.AlternativeSecurityIds.Length -join ';')
     Add-Member -InputObject $cols -MemberType NoteProperty -Name ServiceInfo -Value ($user.ServiceInformation.ServiceInstance -join ';')
     Add-Member -InputObject $cols -MemberType NoteProperty -Name ProxyAddr -Value ($user.ProxyAddresses -join ';')
+    Add-Member -InputObject $cols -MemberType NoteProperty -Name DirSyncProvisioningErrors -Value ($user.DirSyncProvisioningErrors -join ';')
 
     # Add detailed member type info
     Add-Member -InputObject $cols -MemberType NoteProperty -Name UserTypeExtended -Value (Get-MemberTypeExtended -user $user)
 
     # How many days since last password change and since token was last refreshed
     # WARNING: This data does not seem very reliable!
-    Add-Member -InputObject $cols -MemberType NoteProperty -Name PwAge -Value (New-TimeSpan -Start $user.LastPasswordChangeTimestamp).Days -ErrorAction SilentlyContinue
-    Add-Member -InputObject $cols -MemberType NoteProperty -Name StsAge -Value (New-TimeSpan -Start $user.StsRefreshTokensValidFrom).Days -ErrorAction SilentlyContinue
+    try {
+        Add-Member -InputObject $cols -MemberType NoteProperty -Name PwAge -Value (New-TimeSpan -Start $user.LastPasswordChangeTimestamp).Days
+    } catch {
+        Add-Member -InputObject $cols -MemberType NoteProperty -Name PwAge -Value (New-TimeSpan -Start $user.WhenCreated).Days
+    }
+    try {
+        Add-Member -InputObject $cols -MemberType NoteProperty -Name StsAge -Value (New-TimeSpan -Start $user.StsRefreshTokensValidFrom).Days
+    } catch {
+        Add-Member -InputObject $cols -MemberType NoteProperty -Name StsAge -Value (New-TimeSpan -Start $user.WhenCreated).Days
+    }
 
     # --- Track license and service allocation (a license may contain several services) --- #
     # License flags - track which accounts have which licenses assigned - default values first
@@ -298,9 +367,12 @@ function Build-UserRow {
     Add-Member -InputObject $cols -MemberType NoteProperty -Name HasInTune -Value 'N'
     #endregion "ExtraCols"
 
+    # Make sure that street address doesn't contain a \n or \r (they can make a mess of the CSV output)
+    $cols.StreetAddress.replace("`r"," ").replace("`n"," ")
+
     # For each license assigned to this acct...
     Track-LicensesAndServices -licenses $user.Licenses -cols $cols
-
+    
     return $cols
 } # ---- End of function Build-UserRow ---- #
 
@@ -323,7 +395,7 @@ Connect-MsolService -Credential $credential
 #endregion "Login" # -------------------------------------------------------------
 
 
-Get-MsolUser -MaxResults 5 | ForEach-Object `
+Get-MsolUser -All | ForEach-Object `
     -Begin {
         Write-Verbose 'Start processing users'
     } `
@@ -334,10 +406,40 @@ Get-MsolUser -MaxResults 5 | ForEach-Object `
         Write-Verbose 'Finished processing users'
     }
 
+Write-Output ("Number of records created: {0}" -f ($usersOut | measure).Count )
+#$usersOut | gm
+#$usersOut | ft
+
+# Save as temp file
+$usersOut | Export-Csv -Path $outFile -NoTypeInformation -Force
+
+# Import PnP commands
+
+# Connect to SP Web
+Connect-SPOnline -Url ("{0}{1}" -f $spoRootURL, $ICTo365AuditSite) -Credentials $credential
+
+# Load temp file to SP (using PnP)
+Write-Output "Writing $outFile to $ICTo365AuditFolder"
+Add-SPOFile -Path $outFile -Folder $ICTo365AuditFolder
+
+# Rename local file to dated version
+Write-Output "Moving $outFile to $outFileDated"
+Move-Item -Path $outFile -Destination $outFileDated
+
+# Create a dated copy for further analysis (e.g. changes over time)
+Write-Output "Writing $outFileDated to $ICTo365AuditFolder"
+Add-SPOFile -Path $outFileDated -Folder $ICTo365AuditFolder
+
+# Tidy Up
+Write-Output "Tidying up (Disconnecting from SPOnline)"
+Disconnect-SPOnline
+# Let the end of the script destroy the files so we can 
+# check if the script is looping (which can be a problem with AA)
+#Remove-Item $outFile -Force -ErrorAction SilentlyContinue # just in case the first upload failed
+#Remove-Item $outFileDated -Force -ErrorAction SilentlyContinue
+
 $strt = get-date
-Write-Verbose ""
-Write-Verbose "Ending $cmdName at $strt"
-Write-Verbose ""
+Write-Output "Ending $cmdName at $strt"
 
 <#
 Get-MsolUser -MaxResults 1 | Get-Member
